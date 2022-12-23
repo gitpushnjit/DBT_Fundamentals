c99035117c34be86db66c9e488111d3d7e6199d4
Pushkar Gadgil <pushkar.gadgil@inteliment.com>
2022-12-23 06:26:56 +0000

    Modified orders_by_day to include order_status

--- analyses/orders_by_day.sql
+++ analyses/orders_by_day.sql
@@ -1,7 +1,28 @@
 with
     orders as (select * from {{ ref('stg_orders') }}),
 
-    daily as (select order_date, count(*) as order_num from orders group by 1)
+    daily as (
+        select
+            order_date,
+            count(*) as order_num
+            {% for order_status in [
+    "return",
+    "completed",
+    "return_pending",
+    "shipped",
+    "placed",
+] %}
+            sum(
+                case when status = {{ order_status }} then 1 else 0 end
+            ) as {{ order_status }}_total
+            {% endfor %}
+        from orders
+        group by 1
+    ),
+
+    compared as (
+        select *, lag(order_num) over (order by order_date) as previous_day_order
+    )
 
 select *
-from daily
+from compared
