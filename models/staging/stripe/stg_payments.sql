2ab1604fffe57a18220644198bcadecb09d13c66
Pushkar Gadgil <pushkar.gadgil@inteliment.com>
2022-12-26 08:58:50 +0000

    Created a macro cents_to_dollars and called it in stg_payments.sql

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -7,7 +7,7 @@ with
             status,
 
             -- amount is stored in cents, convert it to dollars
-            amount / 100 as amount,
+            {{cents_to_dollars()}} as amount,
             created as created_at
 
         from {{source('stripe','payment')}}
