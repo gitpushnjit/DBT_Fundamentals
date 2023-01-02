66125c49d5d8e93582f22ba7921853d9024f3a1a
Pushkar Gadgil <pushkar.gadgil@inteliment.com>
2023-01-02 10:58:42 +0000

    Modifications to the fct_customer_orders.sql code

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -8,7 +8,30 @@ orders as (
 
 payments as (
     select * from {{ ref('stg_stripe__payments') }} 
+    where payments.payment_status != 'fail'
 ),
+
+-----
+order_totals as (
+    select 
+        order_id,
+        payment_status,
+        sum(payment_amount) as order_value_dollars
+    from payments
+    group by 1,2
+),
+
+order_values_joined as (
+    select
+        orders.*,
+        order_totals.payment_status,
+        order_totals.order_value_dollars
+    from orders
+    left join order_totals
+    on orders.order_id=order_totals.order_id
+),
+
+-----
 
 
 customer_order_history as (
@@ -17,35 +40,29 @@ customer_order_history as (
         customers.full_name,
         customers.surname,
         customers.givenname,
-        min(order_date) as first_order_date,        
+        min(orders.order_date) as first_order_date,        
 
-        min(case 
-        when orders.order_status NOT IN ('returned','return_pending') 
-        then order_date end) 
-        as first_non_returned_order_date,    
+        min(orders.valid_order_date) as first_non_returned_order_date,    
 
-        max(case 
-        when orders.order_status NOT IN ('returned','return_pending')       
-        then order_date end) 
-        as most_recent_non_returned_order_date,       
+        max(orders.valid_order_date) as most_recent_non_returned_order_date,       
 
-        COALESCE(max(user_order_seq),0) as order_count,
+        COALESCE(max(orders.user_order_seq),0) as order_count,
 
         COALESCE(count(case 
-        when orders.order_status != 'returned' 
+        when orders.valid_order_date is not null
         then 1 end),0) 
         as non_returned_order_count,
 
         sum(case 
-        when orders.order_status NOT IN ('returned','return_pending') 
-        then payments.payment_amount else 0 end) 
+        when orders.valid_order_date is not null
+        then orders.order_value_dollars else 0 end) 
         as total_lifetime_value,        
 
         sum(case 
-            when orders.order_status NOT IN ('returned','return_pending') 
-            then payments.payment_amount else 0 end)    
+            when orders.valid_order_date is not null
+            then orders.order_value_dollars else 0 end)    
             /NULLIF(count(case 
-            when orders.order_status NOT IN ('returned','return_pending') 
+            when orders.valid_order_date is not null
             then 1 end),0) 
         as avg_non_returned_order_value,
                 
@@ -54,9 +71,7 @@ customer_order_history as (
     from orders        
     join customers        
     on orders.customer_id = customers.customer_id        
-    left outer join payments         
-    on orders.order_id = payments.order_id        
-    where orders.order_status NOT IN ('pending') and payments.payment_status != 'fail'        
+          
     group by customers.customer_id, customers.full_name, customers.surname, customers.givenname                 
 ),
 
@@ -69,9 +84,9 @@ select
     first_order_date,
     order_count,
     total_lifetime_value,
-    payment_amount as order_value_dollars,
+    orders.order_value_dollars,
     orders.order_status,
-    payments.payment_status
+    orders.payment_status
 from orders
 
 join customers
@@ -80,10 +95,6 @@ on orders.customer_id = customers.customer_id
 join customer_order_history
 on orders.customer_id = customer_order_history.customer_id
 
-left outer join payments
-on orders.order_id = payments.order_id
-
-where payments.payment_status != 'fail'
 )
 
 select * from final
